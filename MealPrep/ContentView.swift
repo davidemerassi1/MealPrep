26d5d242e5f9c22f1016e299af40148f0f7ad022
davide <davide.merassi@studenti.unimi.it>
2026-09-04 11:11:43 +0200

    Initial Commit

---
+++ MealPrep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealPrep
+//
+//  Created by Davide Merassi on 04/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealPrep/MealPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPrepApp.swift
+//  MealPrep
+//
+//  Created by Davide Merassi on 04/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MealPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
